d663f20d0d14066269d64d8f17bdf46a74e8f902
keerthivelp <keerthivel2005@gmail.com>
2026-01-08 12:39:54 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -89,11 +89,11 @@ app.get('*',(req,res)=>{
   })
 })
 
-app.listen(3000,(error) =>{
+app.listen(process.env.PORT || 3000,(error) =>{
     if (error) {
         console.error('Server failed to start');
         console.error(error.message);
         process.exit(1); // stop the app prevent runing the Broken server
     }
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
 })
